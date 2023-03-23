5f86d72846089f4af77347a2c4dd5503b36daa3d
Fractal Hassan <awesomelightning@gmail.com>
2023-03-23 20:42:35 +0000

    Containers are fun. IDK if these work yet, but they're there.

---
+++ App/Node/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const port = 5555;
+app.use(express.static('public'));
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + 'talk.js');
+});
+app.listen(port, function() {
+  console.log('Server running on port ${port}.');
+});
+
+
+

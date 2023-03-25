fb36177b1164d440391e85f78ca3d9a1dd5e7c2e
Fractal Hassan <awesomelightning@gmail.com>
2023-03-25 23:55:52 +0000

    Attempting serverside rendering

--- App/Webpage/server.js
+++ App/Webpage/server.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const ejs = require('ejs');
 const app = express();
 const port = 5555;
 app.use(express.static('public'));
-
-//
+app.set('view engine', 'ejs');
+// Port website will run on
+app.listen(port);
+//https://www.section.io/engineering-education/static-site-dynamic-nodejs-web-app/
 app.get('/', function(req, res) {
   res.sendFile(__dirname + 'app.js');
 });
